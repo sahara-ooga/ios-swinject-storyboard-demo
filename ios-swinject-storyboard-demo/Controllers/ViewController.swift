8613d0fc00f5f3fe8e7c94b7e7daee069ea62e8d
yuu ogasawara <y.ogasawara@st-ventures.jp>
2017-07-09 22:44:49 +0900

    空行・猫の名前の変更

--- ios-swinject-storyboard-demo/Controllers/ViewController.swift
+++ ios-swinject-storyboard-demo/Controllers/ViewController.swift
@@ -21,10 +21,10 @@ class ViewController: UIViewController {
 
     ///AnimalViewControllerにCatオブジェクト(Animalプロトコル)と関連付ける
     private func setupDI() {
+        container.register(Animal.self) { _ in Cat(name: "Mer") }
         container.storyboardInitCompleted(AnimalViewController.self) { r, c in
             c.animal = r.resolve(Animal.self)
         }
-        container.register(Animal.self) { _ in Cat(name: "串田") }
     }
     
     //MARK:-IBAction
@@ -33,8 +33,10 @@ class ViewController: UIViewController {
         let sb = SwinjectStoryboard.create(name: "Main",
                                            bundle: nil,
                                            container: container)
+        
         let vc = sb.instantiateViewController(withIdentifier: "AnimalViewController")
             as! AnimalViewController
+        
         self.present(vc, animated: true, completion: nil)
     }
 }
